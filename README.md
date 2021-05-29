67936bf3cdeee4317820155ed9ba3674e39b8af2
Erwan Pannier <erwan.pannier@gmail.com>
2021-05-29 13:05:49 +0200

    add Fitroom files from Neq library & removed NeQ dependencies

---
+++ README.md
@@ -0,0 +1,8 @@
+# fitroom
+
+
+https://user-images.githubusercontent.com/16088743/119992452-dacb7d00-bfca-11eb-9a4c-d368ae4fdcd7.mp4
+
+
+
+Run the example in [test/test_fitroom.py](./test/test_fitroom.py)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fitroom',
+    version='0.1',
+    url='https://github.com/radis/fitroom',
+    author='Erwan Pannier',
+    author_email='erwan.pannier@gmail.com',
+    description='Interactive Tools for Multi-dimensional fitting of Absorption and Emission Spectra',
+    packages=find_packages(),
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
